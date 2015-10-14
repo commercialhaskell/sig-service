ba4e7d8211697e0ea5f3ad09467a4e3637ee7186
Tim Dysinger <tim@dysinger.net>
2015-10-13 14:19:14 -1000

    add dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,13 @@
-((haskell-mode . ((haskell-indent-spaces . 4)
-                  (haskell-process-use-ghci . t)))
- (hamlet-mode . ((hamlet/basic-offset . 4)
-                 (haskell-process-use-ghci . t))))
+((hamlet-mode
+  . ((hamlet/basic-offset . 4)
+     (haskell-process-use-ghci . t)))
+ (haskell-mode
+  . ((haskell-indent-spaces . 4)
+     (hindent-style . "johan-tibell")
+     (haskell-process-type . ghci)
+     (haskell-process-path-ghci . "stack")
+     (haskell-process-args-ghci . ("ghci"))))
+ (haskell-cabal-mode
+  . ((haskell-process-type . ghci)
+     (haskell-process-path-ghci . "stack")
+     (haskell-process-args-ghci . ("ghci")))))
