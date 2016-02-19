ec4da974a4b11e757bb6a75e8bcf6f985a3ab2b0
Emanuel Borsboom <manny@eb-mbp14a.local>
2016-02-19 10:37:40 -0800

    Helper scripts for CI deployment to multi-AZ Kube
    See https://github.com/fpco/devops-helpers#wrappers

---
+++ etc/build-deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# See description at https://github.com/fpco/devops-helpers#wrappers
+set -xe
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+ENV="$1"; shift
+stack --install-ghc --docker-auto-pull test "$@"
+etc/docker/build.sh --no-build "$@"
+etc/docker/push.sh "$ENV"
+etc/kubernetes/deploy_rc.sh "$ENV"
