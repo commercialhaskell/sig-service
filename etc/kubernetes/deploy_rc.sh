dab0477b470f5369c770cf86d302268c5abea8bc
Emanuel Borsboom <manny@fpcomplete.com>
2016-07-14 23:30:43 -0700

    etc: remove devops-helpers wrapper scripts

--- etc/kubernetes/deploy_rc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# See description at https://github.com/fpco/devops-helpers#wrappers
-set -xe
-exec "$(dirname ${BASH_SOURCE[0]})/../common/devops-helpers/kubernetes/deploy_rc_helper.sh" \
-     --app "sig-service" \
-     --repo commercialhaskell/sig-service \
-     --specdir "$(dirname "${BASH_SOURCE[0]}")" \
-     "$@"
